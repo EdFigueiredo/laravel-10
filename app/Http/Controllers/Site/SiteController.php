e6914f554ff5eefa7996aee5ae26b8e36beb66bf
Edson <edsonelvira@hotmail.com>
2024-12-21 18:15:04 -0300

    Alterações e impletações ao projeto.

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Site;
 class SiteController
 {
     public function contact()
-    {
-        return view('site/contact');
-    }
+        {
+            return view('site/contact');
+        }
 }
