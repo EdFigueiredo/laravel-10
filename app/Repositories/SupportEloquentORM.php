2ef900e45621941fbecd22f7b3090035dc9dd7bf
Edson <edsonelvira@hotmail.com>
2025-01-04 13:17:18 -0300

    feat: Implements store and update repository support

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -4,36 +4,62 @@
 use App\DTO\UpdateSupportDTO;
 use App\DTO\CreateSupportDTO;
 use App\Repositories\SupportRepositoryInterface;
+use App\Models\Support;
 
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
 
+    public function __construct(protected Support $model)
+    {
+        
+    }
+
     public function getAll(string $filter = null): array
     {
-        // Example return value, replace with actual implementation
-        return [];
+       return $this->model
+                    ->where(function ($query) use ($filter){
+                        if($filter){
+                            $query->where('subject', $filter);
+                            $query->orWhere('body', 'like', "%{filter}%");
+                        }
+                    })
+                   ->all()
+                   ->toArray();
     }
 
     public function findOne(string $id): stdClass | null
     {
-        return null;
+        $support = $this->model->find($id);
+        if(!$support){
+            return null;
+        }
+
+        return (object) $support->toArray();
     }
 
     public function delete(string $id): void
     {
-
+        $this->model->findOrFail($id)->delete();
     }
 
     public function new(CreateSupportDTO $dto): stdClass
     {
-        $support = new \stdClass();
-        // Populate $support with data from $dto
-        return $support;
+        
+        $support = $this->model->create((array) $dto);
+
+        return (object) $support->toArray();
     }
 
     public function update(UpdateSupportDTO $dto): stdClass | null
     {
-        return null;
+        if(!$support = $this->model->find($dto->id))
+        {
+            return null;
+        }
+        
+        $support->update((array) $dto);
+
+        return (object) $support->toArray();
     }
 }
